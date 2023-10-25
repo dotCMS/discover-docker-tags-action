c9b0d20be57e3aa3643cf467c355e4be5ea292c9
Victor Alfaro <victor.alfaro@dotcms.com>
2023-10-25 13:44:19 -0600

    Adding formatted output

--- dist/index.js
+++ dist/index.js
@@ -217,10 +217,12 @@ const discoverer = __importStar(__nccwpck_require__(886));
 function run() {
     // Call module logic to discover tags
     const tags = discoverer
-        .discoverTags(core.getInput('version'), core.getInput('hash'), core.getInput('label'), core.getInput('update_stable'), core.getInput('also_latest') === 'true', parseInt(core.getInput('base_tag_size')), core.getInput('image_name'))
-        .join(',');
-    core.info(`Found these tags: [ ${tags} ]`);
-    core.setOutput('discovered_tags', tags);
+        .discoverTags(core.getInput('version'), core.getInput('hash'), core.getInput('label'), core.getInput('update_stable'), core.getInput('also_latest') === 'true', parseInt(core.getInput('base_tag_size')), core.getInput('image_name'));
+    const tagsStr = tags.join(',');
+    const formattedTags = tags.map(tag => `\`${tag}\``).join(', ');
+    core.info(`Found these tags: [ ${tagsStr} ]`);
+    core.setOutput('discovered_tags', tagsStr);
+    core.setOutput('formatted_tags', formattedTags);
 }
 // Run main function
 run();
--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,21 @@ import * as discoverer from './discover-docker-tags'
  */
 function run() {
   // Call module logic to discover tags
-  const tags = discoverer
-    .discoverTags(
-      core.getInput('version'),
-      core.getInput('hash'),
-      core.getInput('label'),
-      core.getInput('update_stable'),
-      core.getInput('also_latest') === 'true',
-      parseInt(core.getInput('base_tag_size')),
-      core.getInput('image_name')
-    )
-    .join(',')
+  const tags = discoverer.discoverTags(
+    core.getInput('version'),
+    core.getInput('hash'),
+    core.getInput('label'),
+    core.getInput('update_stable'),
+    core.getInput('also_latest') === 'true',
+    parseInt(core.getInput('base_tag_size')),
+    core.getInput('image_name')
+  )
+  const tagsStr = tags.join(',')
+  const formattedTags = tags.map(tag => `\`${tag}\``).join(', ')
 
-  core.info(`Found these tags: [ ${tags} ]`)
-  core.setOutput('discovered_tags', tags)
+  core.info(`Found these tags: [ ${tagsStr} ]`)
+  core.setOutput('discovered_tags', tagsStr)
+  core.setOutput('formatted_tags', formattedTags)
 }
 
 // Run main function
